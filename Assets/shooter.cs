ad1c0adcd581347d701629a2d3204e9fab227687
zaka2301 <zaka2301@gmail.com>
2024-01-17 21:15:29 +0700

    Added Mod Toggle

--- Assets/shooter.cs
+++ Assets/shooter.cs
@@ -6,14 +6,20 @@ public class shooter : MonoBehaviour
 {
     [SerializeField] GameObject bullet;
     public float ROF;
+    private BulletModToggle toggle;
     [HideInInspector]
     public float def_rof;
     private float timer;
+    private large_shot large;
+    private Enemy_tracking tracking;
+    private charge_shot charge;
 
     void Start()
     {
         def_rof = ROF;
         spawn();
+        
+
     }
     void Update()
     {
@@ -30,6 +36,15 @@ public class shooter : MonoBehaviour
     }
     void spawn()
     {
+        
         Instantiate(bullet, new Vector3(transform.position.x, transform.position.y, 0), transform.rotation);
+        large = bullet.GetComponent<large_shot>();
+        tracking = bullet.GetComponent<Enemy_tracking>();
+        charge = bullet.GetComponent<charge_shot>();
+
+        large.enabled = BulletModToggle.LargeToggle;
+        tracking.enabled = BulletModToggle.TrackingToggle;
+        charge.enabled = BulletModToggle.ChargeToggle;
+
     }
 }
