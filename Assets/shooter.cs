aa641f5ce9926ecc6e7bc15cb3d12332531e9a5e
zaka2301 <zaka2301@gmail.com>
2024-01-18 20:27:31 +0700

    Add Audio and Background

--- Assets/shooter.cs
+++ Assets/shooter.cs
@@ -6,16 +6,19 @@ public class shooter : MonoBehaviour
 {
     [SerializeField] GameObject bullet;
     public float ROF;
+    [SerializeField] AudioClip ShootSound;
     [HideInInspector]
     public float def_rof;
     private float timer;
     private large_shot large;
     private Enemy_tracking tracking;
     private charge_shot charge;
+    private AudioSource audioSource;
 
     void Start()
     {
         def_rof = ROF;
+        audioSource = GetComponent<AudioSource>();
         spawn();
 
 
@@ -33,8 +36,11 @@ public class shooter : MonoBehaviour
         }
         else
         {
+            
             spawn();
             timer = 0;
+            audioSource.clip = ShootSound;
+            audioSource.Play();
         }
 
     }
@@ -42,6 +48,7 @@ public class shooter : MonoBehaviour
     {
 
         Instantiate(bullet, new Vector3(transform.position.x, transform.position.y, 0), transform.rotation);
+
         large = bullet.GetComponent<large_shot>();
         tracking = bullet.GetComponent<Enemy_tracking>();
         charge = bullet.GetComponent<charge_shot>();
